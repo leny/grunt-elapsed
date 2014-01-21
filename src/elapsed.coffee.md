e2c8c8ebe253496423d3c6f3b2edc1ca94b2839d
Leny <info@flatland.be>
2014-01-21 13:48:08 +0100

    Writing task code

--- src/elapsed.coffee.md
+++ src/elapsed.coffee.md
@@ -9,13 +9,40 @@ Licensed under the MIT license.
 
     "use strict"
 
+    OS = require "os"
+    exec = require( "child_process" ).exec
+    chalk = require "chalk"
+
+Define task
+
     module.exports = ( grunt ) ->
 
-        grunt.registerMultiTask "elapsed", "Compute approximate development time passed on a project, using logs from version control system.", ->
-            options = @options
-                system: "git"
-                cwd: no
-                gap: 120
-                user: no
-
-            grunt.log.writeln "TODO"
+        grunt.registerTask "elapsed", "Compute approximate development time passed on a project, using logs from version control system.", ( system = "git", gap = 120, user = no ) ->
+            done = @async()
+            commands =
+                git:
+                    logs: "git log"
+                    filter: " --author %s"
+                    dateKey: "Date:"
+                hg:
+                    logs: "hg log"
+                    filter: " -u %s"
+                    dateKey: "date:"
+            return grunt.log.error "Unknown version control system '#{ system }' !" unless commands[ system ]
+            command = commands[ system ].logs
+            command += commands[ system ].filter.replace "%s", user if user
+            exec command, ( error, stdOut, stdErr ) ->
+                return grunt.log.error error if error
+                total = 0
+                prev = no
+                gap *= 60000
+                for sLine in stdOut.split( OS.EOL ).reverse()
+                    if sLine.search( commands[ system ].dateKey ) isnt -1
+                        current = ( new Date( sLine.substr( commands[ system ].dateKey.length ).trim() ) ).getTime()
+                        total += difference if prev and prev < current and ( difference = current - prev ) < gap
+                        prev = current
+                total /= 1000
+                minutes = if ( minutes = Math.floor( total / 60 ) ) > 60 then ( minutes / 60 ) % 60 else minutes
+                hours = Math.floor total / 3600
+                grunt.log.writeln "Time elapsed on project: ±#{ chalk.yellow( hours ) } hours, #{ chalk.yellow( minutes ) } minutes."
+                done()
--- tasks/elapsed.js
+++ tasks/elapsed.js
@@ -1,13 +1,68 @@
 "use strict";
+var OS, chalk, exec;
+
+OS = require("os");
+
+exec = require("child_process").exec;
+
+chalk = require("chalk");
+
 module.exports = function(grunt) {
-  return grunt.registerMultiTask("elapsed", "Compute approximate development time passed on a project, using logs from version control system.", function() {
-    var options;
-    options = this.options({
-      system: "git",
-      cwd: false,
-      gap: 120,
-      user: false
+  return grunt.registerTask("elapsed", "Compute approximate development time passed on a project, using logs from version control system.", function(system, gap, user) {
+    var command, commands, done;
+    if (system == null) {
+      system = "git";
+    }
+    if (gap == null) {
+      gap = 120;
+    }
+    if (user == null) {
+      user = false;
+    }
+    done = this.async();
+    commands = {
+      git: {
+        logs: "git log",
+        filter: " --author %s",
+        dateKey: "Date:"
+      },
+      hg: {
+        logs: "hg log",
+        filter: " -u %s",
+        dateKey: "date:"
+      }
+    };
+    if (!commands[system]) {
+      return grunt.log.error("Unknown version control system '" + system + "' !");
+    }
+    command = commands[system].logs;
+    if (user) {
+      command += commands[system].filter.replace("%s", user);
+    }
+    return exec(command, function(error, stdOut, stdErr) {
+      var current, difference, hours, minutes, prev, sLine, total, _i, _len, _ref;
+      if (error) {
+        return grunt.log.error(error);
+      }
+      total = 0;
+      prev = false;
+      gap *= 60000;
+      _ref = stdOut.split(OS.EOL).reverse();
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        sLine = _ref[_i];
+        if (sLine.search(commands[system].dateKey) !== -1) {
+          current = (new Date(sLine.substr(commands[system].dateKey.length).trim())).getTime();
+          if (prev && prev < current && (difference = current - prev) < gap) {
+            total += difference;
+          }
+          prev = current;
+        }
+      }
+      total /= 1000;
+      minutes = (minutes = Math.floor(total / 60)) > 60 ? (minutes / 60) % 60 : minutes;
+      hours = Math.floor(total / 3600);
+      grunt.log.writeln("Time elapsed on project: ±" + (chalk.yellow(hours)) + " hours, " + (chalk.yellow(minutes)) + " minutes.");
+      return done();
     });
-    return grunt.log.writeln("TODO");
   });
 };
